7d60430dcb76cca9fc71199a3d502f498617a8a7
Khanh <khanhtx@nal.vn>
2019-04-26 16:09:48 +0700

    Update using Assert static import

--- src/test/java/vn/nal/unittest/model/CustomerTest.java
+++ src/test/java/vn/nal/unittest/model/CustomerTest.java
@@ -1,7 +1,9 @@
 package vn.nal.unittest.model;
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class CustomerTest {
 
@@ -10,26 +12,26 @@ public class CustomerTest {
         Customer customer = new Customer();
         customer.setFirstName("Jon");
         customer.setLastName("Snow");
-        Assert.assertEquals("Jon Snow", customer.getFullName());
+        assertEquals("Jon Snow", customer.getFullName());
     }
 
     @Test
     public void getFullName_ValidFullName_IfFirstNameIsNull() {
         Customer customer = new Customer();
         customer.setLastName("Snow");
-        Assert.assertEquals("Snow", customer.getFullName());
+        assertEquals("Snow", customer.getFullName());
     }
 
     @Test
     public void getFullName_ValidFullName_IfLastNameIsNull() {
         Customer customer = new Customer();
         customer.setFirstName("Jon");
-        Assert.assertEquals("Jon", customer.getFullName());
+        assertEquals("Jon", customer.getFullName());
     }
 
     @Test
     public void getFullName_ValidFullName_IfLastNameAndLastNameAreNull() {
         Customer customer = new Customer();
-        Assert.assertNull(customer.getFullName());
+        assertNull(customer.getFullName());
     }
 }
